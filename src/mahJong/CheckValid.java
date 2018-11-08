9d46708378981ef0eda01e38bdc3acc601e5a373
Trevor Wong <trevor1997hk@gmail.com>
2018-11-08 20:23:18 +0800

    2018-11-8

--- src/mahJong/CheckValid.java
+++ src/mahJong/CheckValid.java
@@ -17,11 +17,26 @@ public class CheckValid {
 		}
 	}
 	
+	public boolean isValidTile(String tile) {
+		if(tile.charAt(0) == 'T' || tile.charAt(0) == 'S' || tile.charAt(0) == 'M') {
+			int rank = Character.getNumericValue(tile.charAt(1));
+			if(rank >= 1 && rank <= 9) {
+				return true;
+			}
+		} else if ( tile.charAt(0) == 'H' ) {
+			int rank = Character.getNumericValue(tile.charAt(1));
+			if(rank >= 1 && rank <= 7) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public boolean isValidEyes(String eyes[]) {
 		return eyes[0] == eyes[1];
 	}
 	
-	public boolean isValidTile(String tile[]) {
+	public boolean isValidMeld(String tile[]) {
 		int count = 0;
 		for(int i = 0; i < 2; i++) {
 			if(tile[0].charAt(0) == tile[i + 1].charAt(0)) {
@@ -44,17 +59,20 @@ public class CheckValid {
 	}
 	
 	public boolean isValidInput(String mahJong[]) {
+		for(int i = 0; i < Array.getLength(mahJong); i++) {
+			if(!isValidTile(mahJong[i])) {
+				return false;
+			}
+			if(!sameMahJongLessThanFour(mahJong, mahJong[i])) {
+				return false;
+			}
+		}
 		String firstTile[] = new String[]{mahJong[0], mahJong[1], mahJong[2]};
 		String secondTile[] = new String[]{mahJong[3], mahJong[4], mahJong[5]};
 		String thirdTile[] = new String[]{mahJong[6], mahJong[7], mahJong[8]};
 		String fourthTile[] = new String[]{mahJong[9], mahJong[10], mahJong[11]};
 		String eyes[] = new String[]{mahJong[12], mahJong[13]};
-		for(int i = 0; i < Array.getLength(mahJong); i++) {
-			if(!sameMahJongLessThanFour(mahJong, mahJong[i])) {
-				return false;
-			}
-		}
-		return isValidTile(firstTile) && isValidTile(secondTile) && isValidTile(thirdTile) && isValidTile(fourthTile) && isValidEyes(eyes);
+		return isValidMeld(firstTile) && isValidMeld(secondTile) && isValidMeld(thirdTile) && isValidMeld(fourthTile) && isValidEyes(eyes);
 	}
 
 }
