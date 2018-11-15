4c8c84b696396c7b45c2f90f3f327d4b294b04ce
klssss <44697777+klssss@users.noreply.github.com>
2018-11-15 16:16:20 +0800

    Update CalculatorTest.java

--- src/testcase/CalculatorTest.java
+++ src/testcase/CalculatorTest.java
@@ -11,36 +11,52 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 	
 	 @Test
-    public void test1() {
+    	 public void testFan1() { // Big Dragon
 		 String[] input = new String[] {"H5", "H5", "H5", "M1", "M2", "M3", "H6", "H6", "H6", "H7", "H7", "H7", "T1", "T1"}; //
 		 Calculator calculator = new Calculator(input);
 		 int point = calculator.getWinPoint();
 		 assertEquals(8, point);
-    }
+         }
 	 
 	 @Test
-	 public void test2() {
-		 String[] input = new String[] {"M1", "M2", "M3", "M1", "M2", "M3", "T1", "T2", "T3", "S4", "S5", "S6", "T1", "T1"}; //
-		 Calculator calculator = new Calculator(input);
-		 int point = calculator.getWinPoint();
-		 assertEquals(1, point);
-    }
-	 
-	 @Test
-	 public void test3() {
+	 public void testName1() {
 		 String[] input = new String[] {"H5", "H5", "H5", "M1", "M2", "M3", "H6", "H6", "H6", "H7", "H7", "H7", "T1", "T1"}; //
 		 Calculator calculator = new Calculator(input);
 		 String name = calculator.getWinName();
 		 assertEquals("Big Dragon ", name);
 	 }
+	
+	 @Test
+	 public void testFan2() { // Common Hand
+		 String[] input = new String[] {"M1", "M2", "M3", "M1", "M2", "M3", "T1", "T2", "T3", "S4", "S5", "S6", "T1", "T1"}; //
+		 Calculator calculator = new Calculator(input);
+		 int point = calculator.getWinPoint();
+		 assertEquals(1, point);
+         }
 	 
 	 @Test
-	 public void test4() {
+	 public void testName2() { 
 		 String[] input = new String[] {"H5", "H5", "H5", "M1", "M1", "M1", "H6", "H6", "H6", "H7", "H7", "H7", "T1", "T1"}; //
 		 Calculator calculator = new Calculator(input);
 		 String name = calculator.getWinName();
 		 assertEquals("Big Dragon All Triplet Mix Orphans ", name);
 	 }
+	
+	 @Test
+	 public void testFan3() { // All in Triplets
+		 String[] input = new String[] {"M8", "M8", "M8", "T2", "T2", "T2", "S4", "S4", "S4", "T7", "T7", "T7", "H3", "H3"}; //
+		 Calculator calculator = new Calculator(input);
+		 int point = calculator.getWinPoint();
+		 assertEquals(3, point);
+         }
+
+	 @Test
+	 public void testName3() { // All in Triplets
+		 String[] input = new String[] {"M8", "M8", "M8", "T2", "T2", "T2", "S4", "S4", "S4", "T7", "T7", "T7", "H3", "H3"}; //
+		 Calculator calculator = new Calculator(input);
+		 String name = calculator.getWinName();
+		 assertEquals("All Triplet ", name);
+         }
 	 
 
 }
