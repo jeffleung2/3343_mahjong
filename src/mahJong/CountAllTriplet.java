ab769f40723be0eed9ad68280f2276556a9b9e96
klssss <44697777+klssss@users.noreply.github.com>
2018-11-15 16:23:12 +0800

    Update CountAllTriplet.java

--- src/mahJong/CountAllTriplet.java
+++ src/mahJong/CountAllTriplet.java
@@ -2,12 +2,12 @@ package mahJong;
 
 import java.util.ArrayList;
 
-public class CountAllTriplet implements Counter {
+public class CountAllInTriplet implements Counter {
 
 	@Override
 	public void count() {
 		PointsAccumulator pointsAccumulator = WinTracker.getPointsAccumulator();
-		pointsAccumulator.addName("All Triplet ");
+		pointsAccumulator.addName("All in Triplet ");
 		pointsAccumulator.addPoint(3);
 	}
 
