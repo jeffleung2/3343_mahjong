7167bbb2b7f5be3a0a61e43220ca3bb3e14415e2
jonanana98 <44300665+jonanana98@users.noreply.github.com>
2018-11-17 12:35:52 +0800

    Update the wording

--- src/mahJong/CountAllHonorTiles.java
+++ src/mahJong/CountAllHonorTiles.java
@@ -7,7 +7,7 @@ public class CountAllHonorTiles implements Counter {
 	@Override
 	public void count() {
 		PointsAccumulator pointsAccumulator = WinTracker.getPointsAccumulator();
-		pointsAccumulator.setName("All Honor Tiles");
+		pointsAccumulator.setName("All Honor Tiles ");
 		pointsAccumulator.setPoint(13);
 	}
 
